1e6d44e7005f399325f87f658d5b4972d2d3e293
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-18 09:59:44 -0500

    store waves(data) on contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
-        console.log("Inside contract");
+        console.log("Hello from WavePortal");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (unit256) {
+        console.log("%d total waves!", totalWaves);
+        return totalWaves;
     }
 }
