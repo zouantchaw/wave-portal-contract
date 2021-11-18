0424d99fe294e60431f7d2619fb1fd0c15bda18e
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-18 09:47:13 -0500

    explain why hre does not need to be imported

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
+// HRE or Hardhat Runtime Environment is object containing all fuctionality that 
+// Hardhat exposes when running a task, test, or script
+
+// HRE does not need to be imported because when a command starts with 'npx hardhat',
+// the hre object is built on the fly using 'hardhat.config.js'
+
 const main = async () => {
+  // Compiles contract & generates necessary files(under artifacts dir) to work with contract
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  // Hardhat creates a local Ethereum network specifically for this contract
   const waveContract = await waveContractFactory.deploy();
+  // Wait for contract to deploy on local blockchain, 'constructor' runs on deploy
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
 };
