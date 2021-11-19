74c9709f6c7024f7620568d7d0ff77bcf5bd125c
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-19 11:09:35 -0500

    update gitignore

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ const main = async () => {
   const [deployer] = await hre.ethers.getSigners();
   const accountBalance = await deployer.getBalance();
 
-  console.log('Deploying contracts with account: '. deployer.address);
+  console.log('Deploying contracts with account: ', deployer.address);
   console.log('Account balance: ', accountBalance.toString());
 
   const Token = await hre.ethers.getContractFactory('WavePortal');
